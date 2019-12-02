9e09a7014467d0cd6d62e0e541c67ff9c5fe9387
yanxin <yan740487738@qq.com>
2019-12-02 21:40:24 +0800

    modify init.sh v2

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ do
   echo "192.168.12.$i     node$[$i-100]" >> /etc/hosts
 done
 ssh-keygen -f /root/.ssh/id_rsa -N "" &>/dev/null
+rm -f /root/.ssh/known_hosts
+ssh-keyscan -f /etc/hosts > known_hosts 2>/dev/null
+
